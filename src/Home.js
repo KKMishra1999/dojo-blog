258c7328c3b47ab1e5e40d23eaade401c67c3f0c
Koustav Mishra <62167082+KKMishra1999@users.noreply.github.com>
2024-04-17 20:32:36 +0530

    Created custom hook useFetch

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,20 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
+    // const [name, setName] = useState('Mario');
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
-
-    const [name, setName] = useState('Mario');
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
-
-    useEffect(() => {
-        console.log('Use effect ran');
-        console.log(name);
-    }, [name])
+    const {data: blogs, isPending, error} = useFetch("http://localhost:8000/blogs");
+    
 
     return (  
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
             {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs"/> */}
-            <button onClick={() => setName('David')}>change name</button>
-            <p>{name}</p>
+            {/* <button onClick={() => setName('David')}>change name</button>
+            <p>{name}</p> */}
         </div>
     );
 }
